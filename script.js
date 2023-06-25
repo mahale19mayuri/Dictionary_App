9588b611ef683f082710d9b35d838a595e93a6c8
mahale19mayuri <117037168+mahale19mayuri@users.noreply.github.com>
2023-06-25 16:01:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+const input =document.querySelector('input')
+const btn=document.querySelector('button')
+
+
+const dictionary =document.querySelector('.dictionary')
+
+//https://api.dictionaryapi.dev/api/v2/entries/en/<word>
+
+ async function dictionaryFn(word) 
+ {
+    const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    .then(res =>res.json())
+
+    return res[0]
+
+ }
+
+ btn.addEventListener('click',fetchandCreateCard)
+
+    async function fetchandCreateCard(){
+    const data= await dictionaryFn(input.value)
+    console.log(data)
+ 
+
+
+let partOfSpeechArray=[]
+ for(let i=0; i<=data.meanings.length-1; i++){
+    partOfSpeechArray.push(data.meanings[i].partOfSpeech)
+ }
+
+ dictionary.innerHTML=`
+ <div class="card">
+        <div class="property">
+            <span> Word : -</span>
+            <span> ${data.word}</span>
+        </div>
+        <div class="property">
+            <span> Phonetics</span>
+            <span> ${data.phonetic}</span>
+        </div>
+        <div class="property">
+            <span> 
+            <audio controls src='${data.phonetics[0].audio}'></audio>
+            </span>
+        </div>
+        <div class="property">
+            <span> Defination : -</span>
+            <span>${data.meanings[0].definitions[0].definition}</span>
+        </div>
+        <div class="property">
+            <span>${data.meanings[0].definitions[0].example}</span>
+        </div>
+        <div class="property">
+            <span>${partOfSpeechArray.map(e=>e).join(',')}</span>
+        </div>
+</div>`
+    }
+
+
+
+
+
